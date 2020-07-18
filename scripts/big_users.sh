c265b1a5664902e396c49cf82d7667202b0c11a0
hacks4snacks <markdaltongray@gmail.com>
2020-07-18 02:43:05 +0000

    Format testing

--- scripts/big_users.sh
+++ scripts/big_users.sh
@@ -12,7 +12,7 @@ echo "For $CHECK_DIR Directories"
 
 for DIR_CHECK in $CHECK_DIR; do
 	echo ""
-	echo "The $DIR_CHECK Directory:"
-	du -Sh $DIR_CHECK 2>/dev/null | sort -rn | sed '{11,$D; =}' | sed 'N; s/\n/ /' | gawk '{printf $1 ":" "\t" $2 "\t" $3 "\n"}'
+	echo "The "$DIR_CHECK" Directory:"
+	du -Sh "$DIR_CHECK" 2>/dev/null | sort -rn | sed '{11,$D; =}' | sed 'N; s/\n/ /' | gawk '{printf $1 ":" "\t" $2 "\t" $3 "\n"}'
 done
 exit
