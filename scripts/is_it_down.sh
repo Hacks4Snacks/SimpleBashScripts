2046fa29341634fcffcf3dbe6c21f1a2061ba40d
hacks4snacks <markdaltongray@gmail.com>
2020-07-18 01:49:04 +0000

    format changes

--- scripts/is_it_down.sh
+++ scripts/is_it_down.sh
@@ -3,8 +3,8 @@
 # Simple script to test if a website is up
 
 if [ $# -ne 1 ]; then
-  echo "[!] Usage: ./is_it_up website.com"
-  echo "[!] Example: ./is_it_up google.com"
+  echo "[!] Usage: ./is_it_down.sh website.com"
+  echo "[!] Example: ./is_it_down.sh google.com"
 fi
 
 if [ $# -eq 1 ]; then
